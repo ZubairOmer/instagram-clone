dbc1d7a0d491094f8ad326bd5270786179c516aa
Zubair Omer <omer.zubair01@gmail.com>
2021-07-05 23:03:51 -0700

    working with auth of the build

--- src/App.js
+++ src/App.js
@@ -2,10 +2,43 @@ import React, {useState, useEffect} from 'react'
 import './App.css'
 
 import Post from './Post'
-import {db} from './firebase'
+import { db } from './firebase'
+import Modal from '@material-ui/core/Modal'
+import {makeStyles} from '@material-ui/core/styles'
+import { Button, Input } from '@material-ui/core'
+
+function getModalStyle(){
+    const top = 50 
+    const left = 50 
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`
+    }
+}
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+        position: 'absolute',
+        width : 400,
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding : theme.spacing(2,4,3)
+                
+    }
+}))
 
 const App = () => {
+    const classes = useStyles()
+    const [modalStyle] = useState(getModalStyle)
+
     const [posts, setPosts] = useState([])
+    const [open, setOpen] = useState(false)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
 
     useEffect(() => {
         db.collection('posts').onSnapshot(snapshot => {
@@ -13,25 +46,60 @@ const App = () => {
         })
     })
 
+    const signUp = event => {
+        event.preventDefault()
+        
+    }
+
     return (
         <div className='app'>
             <Modal
                 open={open}
-                onClose={handleClose}
+                onClose={() => setOpen(false)}
             >
-                <div style={modalStyle}>
-                    <h2>I am a modal</h2>
+                <div style={modalStyle} className={classes.paper}>
+                    <form  className="app__signup">
+                        <center>
+                             <img 
+                                className="app__headerImage"
+                                height="40px;"
+                                src="https://toogreen.ca/instagreen/img/instagreen.svg"
+                                alt="insta logo"
+                            />
+                         </center>
+
+                        <Input
+                            placeholder='username'
+                            type='text'
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
+                        />
+                        <Input
+                            placeholder='email'
+                            type='text'
+                            value={email}
+                            onChange={e => setEmail( e.target.value)}
+                        />
+                        <Input
+                            placeholder='password'
+                            type='password'
+                            value={password}
+                            onChange={e => setPassword( e.target.value)}
+                        />
+                        <Button type='submit' onClick={signUp}>Sign Up</Button>
+                   </form>
                 </div>
             </Modal>
-            
+
             <div className="app__header">
-                {<img 
+                <img 
                 className="app__headerImage"
                 height="40px;"
                 src="https://toogreen.ca/instagreen/img/instagreen.svg"
                 alt="insta logo"
-              /> }
+              /> 
             </div>
+            <Button onClick={() => setOpen(true)}>Sign Up</Button>
 
             {
                 posts.map(({ id, post }) => (
