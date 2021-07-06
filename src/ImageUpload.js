493cc52dbe1c09a67629f63351f55a60ea702116
Zubair Omer <omer.zubair01@gmail.com>
2021-07-06 03:24:51 -0700

    working on upload

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -4,11 +4,23 @@ import Button from '@material-ui/core/Button'
 
 const ImageUpload = () => {
     const [caption, setCaption] = useState('')
+    const [image, setImage] = useState(null)
+    const [progress, setProgress] = useState(0)
+
+    const handleChange = event => {
+        if (event.files[0]) {
+            setImage(event.target.files[0])
+        }
+    }
+
+    const handleUpload = () => {
+
+    }
 
     return (
         <div>
             <input type="text" placeholder='Enter a caption' onChange={event => setCaption(event.target.value)} />
-            <input type="file"  />
+            <input type="file" onChange={handleChange} />
             <Button onClick={handleUpload}>Upload</Button>
         </div>
     )
