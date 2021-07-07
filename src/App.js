401c02de8d17e446da18db139f0204d02e771567
Zubair Omer <omer.zubair01@gmail.com>
2021-07-07 02:56:52 -0700

    working on comments

--- src/App.js
+++ src/App.js
@@ -138,6 +138,7 @@ const App = () => {
                 posts.map(({ id, post }) => (
                     <Post
                         key={id}
+                        postId={id}
                         username={post.username}
                         caption={post.caption}
                         imageUrl={post.imageUrl}
--- src/Post.js
+++ src/Post.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Post.css'
 
-import {Avatar} from '@material-ui/core'
+import { Avatar } from '@material-ui/core'
+import {db} from './firebase'
 
-const Post = ({ username, caption, imageUrl }) => {
+const Post = ({ username, caption, imageUrl, postId }) => {
+    const [comments, setComments] = useState('')
+    const [comment, setComment] = useState('')
+
+    useEffect(() => {
+        let unsubscribe;
+        if (postId) {
+            unsubscribe = db.collection('posts').docs(postId).collection('commetns').OnSnapshot(snapshot => {
+                setComments(snapshot.docs.map(doc => doc.data()))
+            })
+        }
+
+        return () => {
+            unsubscribe()
+        }
+    }, [postId])
+
     return (
         <div className='post'>
             <div className="post__header">
@@ -21,9 +38,13 @@ const Post = ({ username, caption, imageUrl }) => {
                 alt="avatar"
             />
 
-            <h4
-                className='post__text'><strong>{username}</strong> :{caption}
-            </h4>
+            <h4 className='post__text'><strong>{username}</strong>: {caption} </h4>
+
+            <form>
+                <input type="text" className='post__input'
+                    placeholder='add a comment' value={comment} onChange={e => setComment(e.target.value)} />
+                
+            </form>
         </div>
     )
 }
